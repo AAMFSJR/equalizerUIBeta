f3b5ed551b9d07ec759d405d5ed9f6d60fa6b4b0
AAMFSJR <34865565+AAMFSJR@users.noreply.github.com>
2023-12-09 16:51:19 +0100

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -94,18 +94,18 @@ const Body = () => {
                     description={"Service your investments automatically, while you track performance in your dashboard."}
                 />
 
-                  <div className='pt-10 mb-10 flex items-center justify-center'>
-            { <a href=:'https://blocksurvey.io/equalizer-early-access-7t5z3OqVRC2YEFcUXu8HeA?v=o'> }
-               <button  className='border rounded-2xl px-8 py-5 text-center text-xl font-semibold sm:w-full lg:w-fit border-white bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>
-               REQUEST BETA
-                </button>
-            { </a> }
-        </div>          
+                        
             </section>
         </section>
 
           
-
+<div className='pt-10 mb-10 flex items-center justify-center'>
+                  <figcaption >
+               <button  href=:'https://blocksurvey.io/equalizer-early-access-7t5z3OqVRC2YEFcUXu8HeA?v=o'  className='border rounded-2xl px-8 py-5 text-center text-xl font-semibold sm:w-full lg:w-fit border-white bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>
+               REQUEST BETA
+                </button>
+                 </figcaption>
+        </div>    
 
         
     </section>
