d8b242e4db14dace901fbaff9bd1d340fbbfd636
AAMFSJR <34865565+AAMFSJR@users.noreply.github.com>
2023-12-09 16:48:26 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,14 @@ const Header = () => {
               <a href='#documentation' className='bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>Documentation (Soon)</a>
             </figcaption>
         </section>
-      
+
+    <nav className='flex flex-end  w-full pb-16'>
       <section className='flex flex-end'>
            <figcaption  className=' font-dmsans border border-custom1 rounded-lg px-1 lg:px-8 py-3 lg:py-3 text-center text-sm font-semibold sm:w-full lg:w-fit cursor-default select-none '>
               <button href='https://blocksurvey.io/equalizer-early-access-7t5z3OqVRC2YEFcUXu8HeA?v=o'>REQUEST BETA</button>
             </figcaption>
+      </nav>
+      
         </section>
             <figure className='w-7/12 pl-10 pt-32'>
                 <img src={logo} alt='Logo' className=' block sm:w-28 w-20  md:w-28 lg:w-28 select-none'/>
