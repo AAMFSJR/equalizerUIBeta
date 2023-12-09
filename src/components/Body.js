22aa62cd9902ad93e804d7823a07d38604660e59
AAMFSJR <34865565+AAMFSJR@users.noreply.github.com>
2023-12-09 17:55:59 +0100

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -97,6 +97,11 @@ const Body = () => {
                         
         </section>
 
+         <div className='pt-10 mb-10 flex items-center justify-center'>
+               <figcaption className='border rounded-lg lg:px-8 px-1 lg:py-3 text-center text-base lg:text-xl font-semibold sm:w-full lg:w-fit border-white '>
+              <a href='https://blocksurvey.io/equalizer-early-access-7t5z3OqVRC2YEFcUXu8HeA?v=o' className='bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>REQUEST BETA</a>
+            </figcaption>
+        </div>   
         
     </section>
   )
