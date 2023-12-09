654f2e34ed46b88596584eeb53a3dcaf4adc0c52
AAMFSJR <34865565+AAMFSJR@users.noreply.github.com>
2023-12-09 15:15:18 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,9 @@ const Header = () => {
             <figcaption className='border rounded-lg lg:px-8 px-1 lg:py-3 text-center text-base lg:text-xl font-semibold sm:w-full lg:w-fit border-white '>
               <a href='#documentation' className='bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>Documentation (Soon)</a>
             </figcaption>
+            <figcaption className='border rounded-2xl px-8 py-5 text-center text-xl font-semibold sm:w-full lg:w-fit border-white  bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent border-x-4 border-y-4 '>
+              <a href='https://blocksurvey.io/equalizer-early-access-7t5z3OqVRC2YEFcUXu8HeA?v=o' className='bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'> REQUEST BETA</a>
+            </figcaption>
         </section>
             <figure className='w-7/12 pl-10 pt-32'>
                 <img src={logo} alt='Logo' className=' block sm:w-28 w-20  md:w-28 lg:w-28 select-none'/>
