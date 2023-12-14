37f84382e4d7d92954503d651fe9bde308ec846c
AAMFSJR <34865565+AAMFSJR@users.noreply.github.com>
2023-12-14 14:11:02 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,11 @@ const Header = () => {
             <figcaption className=' border rounded-lg lg:px-8 px-1 lg:py-3 text-center text-base lg:text-xl font-semibold sm:w-full lg:w-fit border-white '>
               <a href='#documentation' className='bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>Documentation (Soon)</a>
             </figcaption>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <button className='border rounded-lg lg:px-8 px-1 lg:py-3 text-center text-base lg:text-xl font-semibold sm:w-full lg:w-fit border-white '>
+              <a href='https://blocksurvey.io/equalizer-early-access-7t5z3OqVRC2YEFcUXu8HeA?v=o' className='bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>REQUEST BETA</a>
+      </button>
+      </div>
          </section>
      
 
@@ -19,11 +24,7 @@ const Header = () => {
                 <img src={logo} alt='Logo' className=' block sm:w-28 w-20  md:w-28 lg:w-28 select-none'/>
             </figure>
 
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <button className='border rounded-lg lg:px-8 px-1 lg:py-3 text-center text-base lg:text-xl font-semibold sm:w-full lg:w-fit border-white '>
-              <a href='https://blocksurvey.io/equalizer-early-access-7t5z3OqVRC2YEFcUXu8HeA?v=o' className='bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>REQUEST BETA</a>
-      </button>
-      </div>
+          
      </nav>
      </>  
 
