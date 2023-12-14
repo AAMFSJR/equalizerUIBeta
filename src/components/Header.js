465bbdabddca85cdce5df6502a1035a4fd724898
AAMFSJR <34865565+AAMFSJR@users.noreply.github.com>
2023-12-14 15:00:33 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
             </figure>
       
       <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <button className='border rounded-lg lg:px-8 px-1 lg:py-3 text-center text-base lg:text-xl font-semibold sm:w-full lg:w-fit border-white '>
+      <button className='border rounded-lg lg:px-8 px-1 lg:py-3 text-center text-base lg:text-xl font-semibold border-white '>
               <a href='https://blocksurvey.io/equalizer-early-access-7t5z3OqVRC2YEFcUXu8HeA?v=o' className='bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>REQUEST BETA</a>
       </button>
       </div>
