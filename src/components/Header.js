bd0ac19c62c40119791844412d75c73e64540371
AAMFSJR <34865565+AAMFSJR@users.noreply.github.com>
2023-12-14 15:23:21 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,13 +17,11 @@ const Header = () => {
                 <img src={logo} alt='Logo' className=' block sm:w-28 w-20  md:w-28 lg:w-28 select-none'/>
             </figure>
 
-       <nav className='flex justify-between  w-full pb-16'>
             <div style={{ display: 'flex', justifyContent: 'flex-end', width: '50px', height: '25px' }}>
       <button className='border rounded-lg lg:px-8 px-1 lg:py-3 text-center text-base lg:text-xl font-semibold sm:w-full lg:w-fit border-white '>
               <a href='https://blocksurvey.io/equalizer-early-access-7t5z3OqVRC2YEFcUXu8HeA?v=o' className='bg-gradient-to-r from-custom6 to bg-custom5 bg-clip-text text-transparent'>REQUEST BETA</a>
       </button>
       </div>
-      </nav>
   
          </section>
      
